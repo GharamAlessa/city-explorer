1db06413a30a3568c1e18ef474f65993748f9f26
GharamAlessa <gharam.alessa@gmail.com>
2021-09-14 21:39:11 +0300

    finish lab06

--- src/App.js
+++ src/App.js
@@ -1,13 +1,78 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Row, Col, Form,Button } from "react-bootstrap";
+import axios from "axios";
 
 export class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello my name is Gharam</h1>
-            </div>
-        )
+constructor(props) {
+    super(props)
+
+    this.state = {
+         longitude:null,
+         latitude:null,
+         name:null
     }
 }
 
-export default App
+handleSubmit=(event)=>{
+event.preventDefault();
+let cityName=event.target.cityName.value;
+console.log(cityName)
+axios.get(`https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY}&q=${cityName}&format=json`)
+.then(result=>{
+    console.log(result.data[0])
+this.setState({
+    longitude:result.data[0].lon,
+    latitude:result.data[0].lat,
+    name:result.data[0].display_name
+})
+console.log(`${this.state.longitude},${this.state.latitude}`)
+})
+
+
+}
+  render() {
+    return (
+
+      <Container>
+        <Row className='text-center'>
+          <h1 className='text-danger'>City explorer</h1>
+        </Row>
+        <Row>
+            <Col md={3}>
+          <Form onSubmit={this.handleSubmit} >
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Enter City</Form.Label>
+              <Form.Control type="text" placeholder="Enter city" name="cityName" />
+              
+            </Form.Group>
+
+           
+            <Button variant="warning" type="submit">
+             explore
+            </Button>
+
+          </Form>
+          </Col>
+        </Row>
+        <Row className='mt-5'>
+            <Col>
+<h3>
+    City Name:{this.state.name}
+</h3>
+            </Col>
+            
+            <Col>
+<p className='h2'>longitude is:{this.state.longitude}</p>
+
+            </Col>
+            <Col className='d-flex justify-content-center'>
+<p className='h2'>latitude is:{this.state.latitude}</p>
+            </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default App;
